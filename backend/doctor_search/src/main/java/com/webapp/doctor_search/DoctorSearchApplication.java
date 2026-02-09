c4fabc6951697d848ff0b46af60cc53e39304179
Sai Pranavesh <challasaipranavesh@gmail.com>
2026-02-09 10:47:59 +0400

    backend initialization

---
+++ backend/doctor_search/src/main/java/com/webapp/doctor_search/DoctorSearchApplication.java
@@ -0,0 +1,13 @@
+package com.webapp.doctor_search;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoctorSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoctorSearchApplication.class, args);
+	}
+
+}
---
+++ backend/doctor_search/src/test/java/com/webapp/doctor_search/DoctorSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webapp.doctor_search;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoctorSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
